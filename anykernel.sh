cbe9f5781cfb4063f51cd82f721c946594f90b6d
iambinaytiwari <40681532+iambinaytiwari@users.noreply.github.com>
2023-11-03 11:37:34 +0545

    m51: Initial
    * Signed-Off by: iambinaytiwari <binaytiwari6@gmail.com>
    
    Change-Id: I1bec59f3c0988a7ac0917feef7bac7e83c2c4dab

--- anykernel.sh
+++ anykernel.sh
@@ -4,16 +4,16 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=<---: NoNameKernel For Galaxy M51 :--->
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=m51
+device.name2=M51
+device.name3=
+device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=
@@ -29,8 +29,8 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
 # boot shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-is_slot_device=0;
+block=auto;
+is_slot_device=auto;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
